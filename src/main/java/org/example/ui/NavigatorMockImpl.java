4bf88b65f7e21c2c9ca5199577409e62aa6a1bc5
Nezno8 <zenitechq@gmail.com>
2024-11-07 15:44:03 +0100

    refactor(findAllCityConnections): add cityConnection to list
    Mock implementation.
    
    Issues #6

---
+++ src/main/java/org/example/ui/NavigatorMockImpl.java
@@ -0,0 +1,25 @@
+package org.example.ui;
+
+import org.example.model.City;
+import org.example.model.CityConnection;
+
+import java.util.List;
+
+public class NavigatorMockImpl implements INavigator{
+    @Override
+    public List<CityConnection> findAllCityConnections() {
+        CityConnection cityConnection1 =
+                new CityConnection(1L,2.0,
+                        new City(1L,"Wro",2.0,4.0),
+                        new City(2L,"Poz",2.0,6.0)
+                );
+
+        CityConnection cityConnection2 =
+                new CityConnection(1L,6.0,
+                        new City(1L,"Szcz",8.0,6.0),
+                        new City(2L,"Poz",2.0,6.0)
+                );
+
+        return List.of(cityConnection1,cityConnection2);
+    }
+}
