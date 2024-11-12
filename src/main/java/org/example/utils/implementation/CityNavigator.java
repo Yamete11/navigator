7b814eca86e2d7b7c429507bfaf9234915e01bb3
saddenly <rustemandasov@outlook.com>
2024-11-12 15:47:44 +0100

    CityNavigator now solely for calculations

--- src/main/java/org/example/utils/implementation/CityNavigator.java
+++ src/main/java/org/example/utils/implementation/CityNavigator.java
@@ -1,5 +1,8 @@
 package org.example.utils.implementation;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.example.model.*;
 import org.example.service.*;
 import org.example.service.implementation.*;
@@ -8,35 +11,27 @@ import org.example.utils.Navigator;
 import java.util.*;
 
 public class CityNavigator implements Navigator {
-    private CityService cityService;
-    private CityConnectionService cityConnectionService;
-    private RouteCityService routeCityService;
-    private StartLocationService startLocationService;
-    private EndLocationService endLocationService;
-    private RouteService routeService;
+    private final CityService cityService;
+    private final CityConnectionService cityConnectionService;
 
     public CityNavigator() {
         this.cityService = new CityServiceImpl();
         this.cityConnectionService = new CityConnectionServiceImpl();
-        this.routeCityService = new RouteCityServiceImpl();
-        this.startLocationService = new StartLocationServiceImpl();
-        this.endLocationService = new EndLocationServiceImpl();
-        this.routeService = new RouteServiceImpl();
     }
 
     @Override
-    public Route findRoute(City start, City end) {
+    public List<RouteCity> findRoute(City start, City end) {
         return getRoute(start, end);
     }
 
     @Override
-    public Route findRoute(String firstCity, String secondCity) {
+    public List<RouteCity> findRoute(String firstCity, String secondCity) {
         City start = cityService.getCityByTitle(firstCity).get();
         City end = cityService.getCityByTitle(secondCity).get();
         return getRoute(start, end);
     }
 
-    private Route getRoute(City start, City end) {
+    private List<RouteCity> getRoute(City start, City end) {
         PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparing(Node::getDistance));
         Map<Long, Double> distances = new HashMap<>();
         Map<Long, Long> previousCities = new HashMap<>();
@@ -53,7 +48,7 @@ public class CityNavigator implements Navigator {
             Long currentCityId = currentNode.getId();
 
             if (Objects.equals(currentCityId, end.getId())) {
-                return buildRoute(distances.get(end.getId()), start, end, previousCities);
+                return buildRoute(end, previousCities);
             }
 
             if (visited.contains(currentCityId)) continue;
@@ -61,7 +56,8 @@ public class CityNavigator implements Navigator {
 
             List<CityConnection> neighbors = cityConnectionService.getCityConnectionsByCityId(currentCityId);
             for (CityConnection connection : neighbors) {
-                Long neighborCityId = Objects.equals(connection.getFirstCity().getId(), currentCityId) ? connection.getSecondCity().getId() : connection.getFirstCity().getId();
+                Long neighborCityId = Objects.equals(connection.getFirstCity().getId(), currentCityId) ?
+                        connection.getSecondCity().getId() : connection.getFirstCity().getId();
                 double newDistance = distances.get(currentCityId) + calculateDistance(connection);
 
                 if (newDistance < distances.get(neighborCityId)) {
@@ -74,31 +70,23 @@ public class CityNavigator implements Navigator {
         return null;
     }
 
-    private Route buildRoute(Double distance, City start, City end, Map<Long, Long> previousCities) {
-        Route result = new Route();
-        result.setTotalDistance(distance);
+    private List<RouteCity> buildRoute(City end, Map<Long, Long> previousCities) {
+        List<RouteCity> routeCities = new ArrayList<>();
 
-        StartLocation startLocation = new StartLocation(start);
-        startLocationService.create(startLocation);
-        result.setStartLocation(startLocation);
-
-        EndLocation endLocation = new EndLocation(end);
-        endLocationService.create(endLocation);
-        result.setEndLocation(endLocation);
-
-        routeService.create(result);
-
-        for (Long at = end.getId(), counter = 0L; at != null; at = previousCities.get(at), counter++) {
+        for (Long at = end.getId(), counter = 0L; at != null;
+             at = previousCities.get(at), counter++) {
             City currentCity = cityService.findAll().get((int) (at - 1));
-            RouteCity routeCity = new RouteCity(currentCity, result, counter);
-            routeCityService.create(routeCity);
+            RouteCity routeCity = new RouteCity(currentCity, null, counter);
+            routeCities.add(routeCity);
         }
 
-        return result;
+        return routeCities;
     }
 
     @Override
     public Double calculateDistance(CityConnection cityConnection) {
+        if (cityConnection.getDistance() != 0.0d) return cityConnection.getDistance();
+
         City city1 = cityConnection.getFirstCity();
         City city2 = cityConnection.getSecondCity();
         return Math.sqrt(Math.pow(city2.getX() - city1.getX(), 2) + Math.pow(city2.getY() - city1.getY(), 2));
@@ -109,29 +97,11 @@ public class CityNavigator implements Navigator {
         return cityConnectionService.findAll();
     }
 
+    @Setter
+    @Getter
+    @AllArgsConstructor
     private static class Node {
         private Long id;
         private Double distance;
-
-        public Node(Long id, Double distance) {
-            this.id = id;
-            this.distance = distance;
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public Double getDistance() {
-            return distance;
-        }
-
-        public void setDistance(Double distance) {
-            this.distance = distance;
-        }
     }
 }
