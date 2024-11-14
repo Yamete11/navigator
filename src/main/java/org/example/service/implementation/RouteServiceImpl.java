ade929eda2cddd693c56a6eab6310420c9e13bad
Gleb Ivanov <s22223@pjwstk.edu.pl>
2024-11-14 02:29:14 +0100

    finished connecting service with math

--- src/main/java/org/example/service/implementation/RouteServiceImpl.java
+++ src/main/java/org/example/service/implementation/RouteServiceImpl.java
@@ -2,14 +2,19 @@ package org.example.service.implementation;
 
 import org.example.dao.RouteMapper;
 import org.example.dao.implementation.RouteMapperImpl;
+import org.example.model.City;
 import org.example.model.CityConnection;
 import org.example.model.Route;
 import org.example.model.RouteCity;
 import org.example.service.RouteService;
+import org.example.utils.DistanceCalculator;
+import org.example.utils.implementation.AStarStrategy;
+import org.example.utils.implementation.CityNavigator;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class RouteServiceImpl implements RouteService {
 
@@ -19,6 +24,7 @@ public class RouteServiceImpl implements RouteService {
     private final RouteCityServiceImpl routeCityService;
     private final CityServiceImpl cityService;
     private final CityConnectionServiceImpl cityConnection;
+    private final CityNavigator cityNavigator;
 
     public RouteServiceImpl() {
         this.routeMapper = new RouteMapperImpl();
@@ -27,16 +33,19 @@ public class RouteServiceImpl implements RouteService {
         this.routeCityService = new RouteCityServiceImpl();
         this.cityService = new CityServiceImpl();
         this.cityConnection = new CityConnectionServiceImpl();
+        this.cityNavigator = new CityNavigator();
+        this.cityNavigator.setStrategy(new AStarStrategy());
     }
 
     @Override
     public void create(Route route) {
         startLocationService.create(route.getStartLocation());
         endLocationService.create(route.getEndLocation());
+        List<RouteCity> routeCities = cityNavigator.findRoute(route.getStartLocation().getCity(), route.getEndLocation().getCity());
+        route.setTotalDistance(DistanceCalculator.calculateTotalDistance(routeCities));
         routeMapper.create(route);
-        //Here we have to get the list of RouteCity and calculate the distance
-        /*List<RouteCity> routeCities = new ArrayList<>();
-        routeCityService.createBatch(routeCities);*/
+        routeCities.forEach(routeCity -> routeCity.setRoute(route));
+        routeCityService.createBatch(routeCities);
     }
 
     @Override
