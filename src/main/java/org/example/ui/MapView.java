91388fc1183c5a347e194d78b4298d7a32128646
saddenly <rustemandasov@outlook.com>
2024-11-18 17:18:46 +0100

    upd

--- src/main/java/org/example/ui/MapView.java
+++
@@ -1,7 +0,0 @@
-package org.example.ui;
-
-public class MapView {
-    public static void main(String[] args) {
-       // System.out.println(new GraphDrawer(new NavigatorMockImpl()).draw());
-    }
-}
